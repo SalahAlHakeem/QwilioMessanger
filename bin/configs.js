4de0eb14cbcc557fdc7ad0f359120aec3801ab19
Salahuddin Ruziyev <salahuddinruziyev@Salahuddins-MacBook-Air.local>
2024-05-04 13:40:23 +0500

    changes again

--- bin/configs.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.statusResponses = void 0;
-exports.statusResponses = {
-    EXECUTED: 1,
-    FAILED: 2
-};
--- bin/routes/clientRouter.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.messageRouter = void 0;
-const express_1 = require("express");
-exports.messageRouter = (0, express_1.Router)();
